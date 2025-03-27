19201e04676bffa07010634b633e96a5f0a05737
vchironi <vchironi@gmail.com>
2025-03-27 23:17:37 +0100

    my16th commit to be approved

--- providers.tf
+++ providers.tf
@@ -1,17 +1,5 @@
 
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-  }
-}
-terraform {
   backend "s3" {
     bucket         = "terraform-state-filebuckett"
     key            = "state/terraform.tfstate"
