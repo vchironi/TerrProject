fcabc760c5f355ea3df3994e74505bd8766c4e2e
vchironi <vchironi@gmail.com>
2025-03-27 22:39:59 +0100

    my13th commit to be approved

--- storage.tf
+++ storage.tf
@@ -1,20 +1,4 @@
-provider "aws" {
-  region = "us-east-1"
-}
 
-# Terraform configuration for providers
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-  }
-}
 
 # Generate a unique suffix
 resource "random_string" "suffix" {
