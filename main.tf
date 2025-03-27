09a9f91ebd3b602ea1ca22bd9b930dde771f98e5
vchironi <vchironi@gmail.com>
2025-03-27 23:48:13 +0100

    my18th commit to be approved

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ provider "aws" {
 }
 
 # Create Create an IAM policy and attach it to the user
+# Create Create an  IAM policy and attach it to the user
 resource "aws_iam_policy" "terraform_backend_policy_s3_only" {
   name        = "TerraformBackendS3Only"
   description = "Permissions for Terraform to use S3 backend without locking"
