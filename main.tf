1d15fcd4313e16dd9c1a03631d83bdfd88a16e0d
vchironi <vchironi@gmail.com>
2025-03-27 23:10:54 +0100

    my16th commit to be approved

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucket"
+    bucket         = "terraform-state-filebuckett"
     key            = "state/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
