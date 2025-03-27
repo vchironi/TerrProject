a33de69fbecc8172631a74ce39f3bee1f37f43d1
vchironi <vchironi@gmail.com>
2025-03-27 22:30:52 +0100

    my9th commit to be approved

--- main.tf
+++ main.tf
@@ -1,19 +1,33 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1"  # Adjust to your preferred region
 }
 
+# Create IAM Users
 resource "aws_iam_user" "users" {
-  for_each = toset(var.usernames)
-  name     = each.value
+  for_each = toset(["ARMANDO", "Fucksas"])
+  name     = each.key
 }
 
+# Attach Read-Only EC2 Policy to Users
 resource "aws_iam_user_policy_attachment" "readonly" {
-  for_each       = toset(var.usernames)
-  user           = each.value
-  policy_arn     = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
+  for_each   = aws_iam_user.users
+  user       = each.key
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
 }
 
+# Create Access Keys for Users
 resource "aws_iam_access_key" "keys" {
-  for_each = toset(var.usernames)
-  user     = each.value
+  for_each = aws_iam_user.users
+  user     = each.key
+}
+
+# Optional: Output access keys (be careful with this in production)
+output "user_access_keys" {
+  value = {
+    for k, v in aws_iam_access_key.keys : k => {
+      access_key_id     = v.id
+      secret_access_key = v.secret
+    }
+  }
+  sensitive = true
 }
