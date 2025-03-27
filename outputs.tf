f23be59f66080a3687a2458105ea583800da331c
vchironi <vchironi@gmail.com>
2025-03-27 22:10:52 +0100

    my5th commit to be approved

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
-output "user_access_keys" {
-  value = {
-    for user, key in aws_iam_access_key.keys :
-    user => {
-      access_key_id     = key.id
-      secret_access_key = key.secret
-    }
-  }
+# output "user_access_keys" {
+#   value = {
+#     for user, key in aws_iam_access_key.keys :
+#     user => {
+#       access_key_id     = key.id
+#       secret_access_key = key.secret
+#     }
+#   }
 
-  sensitive = true
-}
+#   sensitive = true
+# }
